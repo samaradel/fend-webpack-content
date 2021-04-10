a43e51765082d280814be2f2f4f553babf58ebc7
samaradel <samar@incubaid.com>
2021-04-10 08:11:00 +0200

    Mode

---
+++ webpack.prod.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.js',
+    module: {
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }, {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+                loader: 'file-loader',
+            }, ],
+        }, ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
+}
